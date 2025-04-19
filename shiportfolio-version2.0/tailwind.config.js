898f26c19f088c66ce39610e6ce74f920925fcbd
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-19 12:37:51 +0630

    commit

--- shiportfolio-version2.0/tailwind.config.js
+++ shiportfolio-version2.0/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', 
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
