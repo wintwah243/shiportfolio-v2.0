46184a05d37b49f671f0a74da51f230adca96d43
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-20 13:02:33 +0630

    Update AboutMe.jsx

--- shiportfolio-version2.0/src/sections/AboutMe.jsx
+++ shiportfolio-version2.0/src/sections/AboutMe.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import LOGO from '../assets/images/LOGO.png'
 import { ABOUT_ME } from '../utils/data'
 
 const AboutMe = () => {
   return (
-    <section id='about' className='container mx-auto px-8 py-16'>
+    <motion.section
+      id='about'
+      className='container mx-auto px-8 py-16'
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6 }}
+    >
       <h4 className='block lg:hidden w-[200px] section-title text-left mb-16'>About Me</h4>
-      <div className='flex flex-col lg:flex-row gap-16 items-start justify-between'>
-        <div className='w-[300px] md:w-[370px] h-[350px] md:h-[428px] mx-auto'>
-          <img src={LOGO} alt='Profile Pic' className='profile-pic' />
-        </div>
 
-        <div className='flex-1'>
+      <div className='flex flex-col lg:flex-row gap-16 items-start justify-between'>
+        <motion.div
+          className='w-[300px] md:w-[370px] h-[350px] md:h-[428px] mx-auto'
+          initial={{ opacity: 0, x: -30 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.6 }}
+        >
+          <img src={LOGO} alt='Profile Pic' className='profile-pic' />
+        </motion.div>
+
+        <motion.div
+          className='flex-1'
+          initial={{ opacity: 0, x: 30 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+        >
           <h4 className='hidden lg:block w-[200px] section-title text-left'>About Me</h4>
           <p className='text-sm text-justify leading-6 whitespace-pre-line mt-4'>{ABOUT_ME.content}</p>
 
@@ -22,9 +43,9 @@ const AboutMe = () => {
               </a>
             ))}
           </div>
-        </div>
+        </motion.div>
       </div>
-    </section>
+    </motion.section>
   )
 }
 
