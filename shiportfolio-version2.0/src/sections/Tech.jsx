099dafc3f295a5eeb9947a54c54402a2796b02f9
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-05-05 13:20:07 +0630

    insert-necessary-comments

--- shiportfolio-version2.0/src/sections/Tech.jsx
+++ shiportfolio-version2.0/src/sections/Tech.jsx
@@ -5,15 +5,21 @@ import SkillCard from '../components/SkillCard';
 import { motion } from "framer-motion";
 
 const Tech = () => {
-  const [activeTab, setActiveTab] = useState('language');
+  //this state is used for activeTab UI
+  const [activeTab, setActiveTab] = useState('language'); 
+  
+  //set tabData initial value to Language
   const [tabData, setTabData] = useState(
-    SKILLS.filter((skill) => skill.type === 'language')
+    // we got skill.type from data.js from utils folder
+    SKILLS.filter((skill) => skill.type === 'language') //initial array with type 'language'
   );
-
+  
+  //event handler to handle tab value change
   const handleTabValueChange = (value) => {
+    //create new array with filter method based on skill type
     const updatedList = SKILLS.filter((skill) => skill.type === value);
-    setTabData(updatedList);
-    setActiveTab(value);
+    setTabData(updatedList); //update tab data
+    setActiveTab(value); //update activeTab UI based on value
   };
 
   return (
@@ -25,6 +31,7 @@ const Tech = () => {
       id='skills'
       className='mt-20 bg-white'
     >
+      {/* Header and Intro */}
       <div className='container mx-auto p-10'>
         <div className='w-full lg:w-[60vw] mx-auto'>
           <h4 className='section-title'>Technical Skills</h4>
@@ -33,6 +40,7 @@ const Tech = () => {
           </p>
         </div>
 
+       {/* Tab Data menu Section */}
         <Tabs
           tabList={SKILL_TABS}
           activeTab={activeTab}
@@ -40,7 +48,8 @@ const Tech = () => {
         />
 
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 min-h-[430px]'>
-          {tabData.map((skill, index) => (
+        {/* use map method to display tab data which is from handleTabValueChange event handler  */}
+          {tabData.map((skill, index) => (  
             <motion.div
               key={skill.id}
               initial={{ opacity: 0, y: 20 }}
