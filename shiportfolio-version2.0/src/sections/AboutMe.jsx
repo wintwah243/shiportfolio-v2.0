3f51dd99733414796310001359e12e3bc16c0f8d
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-05-15 21:24:46 +0630

    modify about me section

--- shiportfolio-version2.0/src/sections/AboutMe.jsx
+++ shiportfolio-version2.0/src/sections/AboutMe.jsx
@@ -34,7 +34,16 @@ const AboutMe = () => {
           transition={{ duration: 0.6, delay: 0.2 }}
         >
           <h4 className='hidden lg:block w-[200px] section-title text-left'>About Me</h4>
-          <p className='text-sm text-justify leading-6 whitespace-pre-line mt-4'>{ABOUT_ME.content}</p>
+          
+          <p className='text-sm text-justify leading-6 whitespace-pre-line mt-4'>
+            You can call me <span className='text-green-600'>"Shi"</span> — a coffee lover with a passion for coding.
+            I describe myself as a person who is always eager to grow and
+            explore new things. As a <span className="text-green-600">full-stack developer </span> 
+            and a <span className="text-green-600">global learning enthusiast</span>, 
+            I often find myself "productively wasting time" by seeking out opportunities, 
+            learning new skills, and challenging myself to improve — because for me, 
+            growth never stops.
+          </p>
 
           <div className='flex gap-4 mt-6'>
             {ABOUT_ME.socialLinks.map((item) => (
@@ -55,4 +64,4 @@ const AboutMe = () => {
   )
 }
 
-export default AboutMe
+export default AboutMe;
