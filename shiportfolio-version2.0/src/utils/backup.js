b24fa0c1f80fa049dcfe64c54a874a49ad083b62
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-05-17 06:47:28 +0630

    Add files via upload

---
+++ shiportfolio-version2.0/src/utils/backup.js
@@ -0,0 +1,6 @@
+export const backupInfo = `
+👋 Hello! I'm Shi. 
+Currently, my chatbot is temporarily unavailable due to exceeding the free tier limit and
+I can't pay bill because I'm broke 😭 , but it will be back soon.
+Thanks for your patience — hope you enjoy your visit!
+`;
