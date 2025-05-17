d51258d16f45f2a4dfc24b546683a677c915425b
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-05-17 16:10:48 +0630

    Update Contact.jsx

--- shiportfolio-version2.0/src/sections/Contact.jsx
+++ shiportfolio-version2.0/src/sections/Contact.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
       </div>
 
       <div className="bg-white py-16 px-6 md:px-12 text-center">
-      <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
+      <h2 className="section-title">
          Reach out to me
         </h2>
         <p className="text-gray-700 mb-8 max-w-xl mx-auto">
