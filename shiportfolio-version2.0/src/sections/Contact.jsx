5cc63ed16b28280328152e78cd80da962694f2f5
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-05-17 15:46:11 +0630

    Update Contact.jsx

--- shiportfolio-version2.0/src/sections/Contact.jsx
+++ shiportfolio-version2.0/src/sections/Contact.jsx
@@ -52,7 +52,7 @@ const Contact = () => {
 
       <div className="bg-white py-16 px-6 md:px-12 text-center">
       <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
-         Direct Message to Me
+         Reach out to me
         </h2>
         <p className="text-gray-700 mb-8 max-w-xl mx-auto">
           Connect with me directly through Telegram or Email for the fastest response.
