e390a3f34a79acb5f52b1826fd08df85d4fe1aad
ocean_shii <wahwint72@gmail.com>
2025-09-14 15:15:16 +0630

    project card ui update

--- shiportfolio-version2.0/src/components/ProjectCard.jsx
+++ shiportfolio-version2.0/src/components/ProjectCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { FaGithub } from 'react-icons/fa'
 
-const ProjectCard = ({imgUrl, title, tags, link}) => {
+const ProjectCard = ({imgUrl, title, tags, link, type}) => {
   return (
     <div className='h-full bg-white rounded-xl overflow-hidden shadow-md mx-2'>
         <img src={imgUrl} alt={title} className='w-full h-72 md:h-80 object-cover' />
@@ -18,11 +19,26 @@ const ProjectCard = ({imgUrl, title, tags, link}) => {
                 ))}
             </div>
 
-          <div className=''>
+          {/* Separator */}
+            <div className="flex items-center my-6">
+                    <hr className="flex-grow border-black" />
+                    <span className="mx-3 text-gray-500 text-sm font-medium"></span>
+                    <hr className="flex-grow border-black" />
+            </div>
+
+          <div className='flex gap-2'>
                     <a href={link} target="_blank" rel="noopener noreferrer" className='cursor-pointer group'>
-                        <button className='mt-2'>View on GitHub</button>
+                        <button className='flex mt-4 text-gray-900 bg-gray-100 border py-2 px-2 rounded-lg'>
+                            Source code
+                            <FaGithub className='ml-2 mt-1' />
+                        </button>
                     </a>
-          </div>
+                    <a href={link} target="_blank" rel="noopener noreferrer" className='cursor-pointer group'>
+                        <button className='flex mt-4 text-blue-700 bg-blue-100 border py-2 px-2 rounded-lg'>
+                            {type}
+                        </button>
+                    </a>
+                </div>
         </div>
     </div>
   )
